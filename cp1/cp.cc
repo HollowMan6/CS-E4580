68398a4a2afc660ccf5c9e394591e20383cd2ce1
Hollow Man <hollowman@opensuse.org>
2023-04-22 00:11:09 +0300

    Finish prereq, mf1, cp1
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- cp1/cp.cc
+++ cp1/cp.cc
@@ -1,3 +1,5 @@
+#include <cmath>
+
 /*
 This is the function you need to implement. Quick reference:
 - input rows: 0 <= y < ny
@@ -6,5 +8,49 @@ This is the function you need to implement. Quick reference:
 - correlation between rows i and row j has to be stored in result[i + j*ny]
 - only parts with 0 <= j <= i < ny need to be filled
 */
-void correlate(int ny, int nx, const float *data, float *result) {
+void correlate(int ny, int nx, const float *data, float *result)
+{
+    double *data_copy = new double[ny * nx];
+    for (int y = 0; y < ny; y++)
+    {
+        // step 1: normalize rows to have mean 0
+        double mean = 0;
+        for (int x = 0; x < nx; x++)
+        {
+            mean += data[x + y * nx];
+        }
+        mean /= nx;
+        double scale = 0;
+        for (int x = 0; x < nx; x++)
+        {
+            data_copy[x + y * nx] = data[x + y * nx] - mean;
+            scale += data_copy[x + y * nx] * data_copy[x + y * nx];
+        }
+
+        // step 2: normalize rows to have length 1
+        scale = sqrt(scale);
+        if (scale == 0)
+        {
+            scale = 1;
+        }
+        for (int x = 0; x < nx; x++)
+        {
+            data_copy[x + y * nx] /= scale;
+        }
+    }
+
+    // step 3: calculate matrix product
+    for (int i = 0; i < ny; i++)
+    {
+        for (int j = i; j < ny; j++)
+        {
+            double dot = 0;
+            for (int x = 0; x < nx; x++)
+            {
+                dot += data_copy[x + i * nx] * data_copy[x + j * nx];
+            }
+            result[j + i * ny] = dot;
+        }
+    }
+    delete[] data_copy;
 }
--- prereq/prereq.cc
+++ prereq/prereq.cc
@@ -1,4 +1,7 @@
-struct Result {
+#include <cmath>
+#include <cstdint>
+struct Result
+{
     float avg[3];
 };
 
@@ -12,7 +15,28 @@ This is the function you need to implement. Quick reference:
 - input: data[c + 3 * x + 3 * nx * y]
 - output: avg[c]
 */
-Result calculate(int ny, int nx, const float *data, int y0, int x0, int y1, int x1) {
+Result calculate(int ny, int nx, const float *data, int y0, int x0, int y1, int x1)
+{
     Result result{{0.0f, 0.0f, 0.0f}};
+
+    int num_pixels = (y1 - y0) * (x1 - x0);
+    double r_sum = 0.0;
+    double g_sum = 0.0;
+    double b_sum = 0.0;
+
+    for (int y = y0; y < y1; ++y)
+    {
+        for (int x = x0; x < x1; ++x)
+        {
+            r_sum += data[3 * (y * nx + x) + 0];
+            g_sum += data[3 * (y * nx + x) + 1];
+            b_sum += data[3 * (y * nx + x) + 2];
+        }
+    }
+
+    result.avg[0] = r_sum / num_pixels;
+    result.avg[1] = g_sum / num_pixels;
+    result.avg[2] = b_sum / num_pixels;
+
     return result;
 }
