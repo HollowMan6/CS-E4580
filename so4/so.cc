4449eca581e4397b4d71796ee8ef07f52d11fd82
Hollow Man <hollowman@opensuse.org>
2023-05-13 17:35:04 +0300

    Init SO4
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- so4/so.cc
+++ so4/so.cc
@@ -1,9 +1,48 @@
 #include <algorithm>
+#include <omp.h>
 
 typedef unsigned long long data_t;
 
-void psort(int n, data_t *data) {
-    // FIXME: Implement a more efficient parallel sorting algorithm for the CPU,
-    // using the basic idea of merge sort.
-    std::sort(data, data + n);
+static inline int last_pow2(int x)
+{
+    return x == 1 ? 1 : 1 << ((64 - 1) - __builtin_clzl(x - 1));
+}
+
+void psort(int n, data_t *data)
+{
+    int threads = last_pow2(omp_get_max_threads());
+
+    if (threads < 2)
+        return std::sort(data, data + n);
+
+    int block = (n + threads - 1) / threads;
+
+#pragma omp parallel for
+    for (int i = 0; i <= threads; i++)
+    {
+        int index = i * block;
+        int start = std::min(index, n);
+        int end = std::min(index + block, n);
+        std::sort(data + start, data + end);
+    }
+
+    // merge pairs of blocks
+    threads /= 2;
+
+    while (threads > 0)
+    {
+#pragma omp parallel for
+        for (int i = 0; i < threads; i++)
+        {
+            int index = i * 2;
+
+            int start = index * block;
+            int middle = std::min(start + block, n);
+            int end = std::min(start + 2 * block, n);
+
+            std::inplace_merge(data + start, data + middle, data + end);
+        }
+        block *= 2;
+        threads /= 2;
+    }
 }
