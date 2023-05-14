6baae3986c18c6f44011263fd50dc09c25ea3c93
Hollow Man <hollowman@opensuse.org>
2023-05-14 21:17:43 +0300

    cp3a
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- cp3a/cp.cc
+++ cp3a/cp.cc
@@ -1,10 +1,12 @@
 #include <cmath>
 #include <vector>
-#include <x86intrin.h>
+#include <immintrin.h>
 
-typedef double double4_t __attribute__((vector_size(4 * sizeof(double))));
-static inline double4_t swap2(double4_t x) { return _mm256_permute2f128_pd(x, x, 0b00000001); }
-static inline double4_t swap1(double4_t x) { return _mm256_permute_pd(x, 0b00000101); }
+typedef double double8_t __attribute__((vector_size(8 * sizeof(double))));
+
+static inline double8_t swap4(double8_t x) { return _mm512_permutexvar_pd(_mm512_set_epi64(3, 2, 1, 0, 7, 6, 5, 4), x); }
+static inline double8_t swap2(double8_t x) { return _mm512_permutex_pd(x, 0b01001110); }
+static inline double8_t swap1(double8_t x) { return _mm512_permute_pd(x, 0b01010101); }
 
 /*
 This is the function you need to implement. Quick reference:
@@ -16,41 +18,18 @@ This is the function you need to implement. Quick reference:
 */
 void correlate(int ny, int nx, const float *data, float *result)
 {
-    double *data_copy = new double[ny * nx];
-
-#pragma omp parallel for
-    for (int y = 0; y < ny; y++)
-    {
-        // step 1: normalize rows to have mean 0
-        double sum = 0;
-        for (int x = 0; x < nx; x++)
-        {
-            sum += data[x + y * nx];
-        }
-        double mean = sum / nx;
-        double scale = 0;
-        for (int x = 0; x < nx; x++)
-        {
-            data_copy[x + y * nx] = data[x + y * nx] - mean;
-            scale += data_copy[x + y * nx] * data_copy[x + y * nx];
-        }
-
-        // step 2: normalize rows to have length 1
-        scale = sqrt(scale);
-        if (scale == 0)
-        {
-            scale = 1;
-        }
-        for (int x = 0; x < nx; x++)
-        {
-            data_copy[x + y * nx] /= scale;
-        }
-    }
-
     // step 3: calculate matrix product
-    const int element = 4;
+    const int element = 8;
     const int ny_v = (ny + element - 1) / element;
-    std::vector<std::vector<double4_t>> data_copy_v(ny_v, std::vector<double4_t>(nx));
+
+    const int nx_v = (nx + element - 1) / element;
+    const int nx_vc = nx_v * element;
+    const int ny_vbr = (ny_v + element - 1) / element;
+    const int ny_vr = ny_vbr * element;
+
+    const double8_t zero = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
+
+    std::vector<std::vector<double8_t>> data_copy_v(ny_vr, std::vector<double8_t>(nx_vc));
 
 #pragma omp parallel for
     for (int y = 0; y < ny_v; y++)
@@ -58,61 +37,124 @@ void correlate(int ny, int nx, const float *data, float *result)
         for (int x = 0; x < nx; x++)
         {
             const int base = y * element;
-            double4_t v = {0.0, 0.0, 0.0, 0.0};
+            double8_t v = zero;
             for (int e = 0; e < element && base + e < ny; e++)
             {
-                v[e] = data_copy[nx * (base + e) + x];
+                v[e] = data[nx * (base + e) + x];
             }
             data_copy_v[y][x] = v;
         }
     }
 
-#pragma omp parallel for schedule(static, 1)
-    for (int x = 0; x < ny_v; ++x)
+    double div_nx = 1 / (double)nx;
+    double8_t div_nx_v = {div_nx, div_nx, div_nx, div_nx, div_nx, div_nx, div_nx, div_nx};
+
+#pragma omp parallel for
+    for (int y = 0; y < ny_v; y++)
     {
-        for (int y = x; y < ny_v; ++y)
+        double8_t sum = zero;
+        double8_t sqr = zero;
+        double8_t norm = zero;
+
+        // step 1: normalize rows to have mean 0
+        for (int x = 0; x < nx; x++)
         {
-            double4_t sums00 = {0.0, 0.0, 0.0, 0.0};
-            double4_t sums01 = {0.0, 0.0, 0.0, 0.0};
-            double4_t sums10 = {0.0, 0.0, 0.0, 0.0};
-            double4_t sums11 = {0.0, 0.0, 0.0, 0.0};
+            sum += data_copy_v[y][x];
+        }
+        double8_t mean = sum * div_nx_v;
+        for (int x = 0; x < nx; x++)
+        {
+            double8_t num = data_copy_v[y][x];
+            double8_t norm = num - mean;
+            data_copy_v[y][x] = norm;
+            sqr += norm * norm;
+        }
+        // step 2: normalize rows to have length 1
+        sqr *= div_nx_v;
+        for (int e = 0; e < element; e++)
+        {
+            if (sqr[e] != 0)
+                norm[e] = 1 / std::sqrt(sqr[e]);
+        }
+        for (int x = 0; x < nx; x++)
+        {
+            data_copy_v[y][x] *= norm;
+        }
+    }
 
-            for (int k = 0; k < nx; ++k)
+// step 3: calculate matrix product
+#pragma omp parallel for schedule(dynamic, 1)
+    for (int x = 0; x < ny_vbr; x++)
+    {
+        for (int y = x; y < ny_vbr; y++)
+        {
+            double8_t sums[element][element][element] = {{{0.0}}};
+            for (int m = 0; m < nx_v; m++)
             {
-                double4_t x00 = data_copy_v[x][k];
-                double4_t y00 = data_copy_v[y][k];
-                double4_t x10 = swap2(x00);
-                double4_t y01 = swap1(y00);
-
-                sums00 += x00 * y00;
-                sums01 += x00 * y01;
-                sums10 += x10 * y00;
-                sums11 += x10 * y01;
+                for (int a = 0; a < element; a++)
+                {
+                    for (int b = 0; b < element; b++)
+                    {
+                        int j = x * element + a;
+                        int i = y * element + b;
+                        double8_t sum000 = zero;
+                        double8_t sum001 = zero;
+                        double8_t sum010 = zero;
+                        double8_t sum011 = zero;
+                        double8_t sum100 = zero;
+                        double8_t sum101 = zero;
+                        double8_t sum110 = zero;
+                        double8_t sum111 = zero;
+                        for (int k = 0; k < element; k++)
+                        {
+                            double8_t x000 = data_copy_v[j][k + m * element];
+                            double8_t y000 = data_copy_v[i][k + m * element];
+                            double8_t x100 = swap4(x000);
+                            double8_t x010 = swap2(x000);
+                            double8_t x110 = swap2(x100);
+                            double8_t y001 = swap1(y000);
+                            sum000 += x000 * y000;
+                            sum001 += x000 * y001;
+                            sum010 += x010 * y000;
+                            sum011 += x010 * y001;
+                            sum100 += x100 * y000;
+                            sum101 += x100 * y001;
+                            sum110 += x110 * y000;
+                            sum111 += x110 * y001;
+                        }
+                        sums[a][b][0] += sum000 * div_nx_v;
+                        sums[a][b][1] += sum001 * div_nx_v;
+                        sums[a][b][2] += sum010 * div_nx_v;
+                        sums[a][b][3] += sum011 * div_nx_v;
+                        sums[a][b][4] += sum100 * div_nx_v;
+                        sums[a][b][5] += sum101 * div_nx_v;
+                        sums[a][b][6] += sum110 * div_nx_v;
+                        sums[a][b][7] += sum111 * div_nx_v;
+                    }
+                }
             }
-
-            double4_t sums[4] = {sums00, sums01, sums10, sums11};
-            for (int e = 1; e < element; e += 2)
-            {
-                sums[e] = swap1(sums[e]);
-            }
-
             for (int a = 0; a < element; a++)
             {
                 for (int b = 0; b < element; b++)
                 {
-                    int i = a + x * element;
-                    int j = b + y * element;
-                    if (i <= j)
+                    for (int kb = 1; kb < element; kb += 2)
                     {
-                        if (j < ny && i < ny)
+                        sums[a][b][kb] = swap1(sums[a][b][kb]);
+                    }
+                    for (int jb = 0; jb < element; jb++)
+                    {
+                        for (int ib = 0; ib < element; ib++)
                         {
-                            result[ny * i + j] = sums[a ^ b][b];
+                            int j = x * element * element + a * element + ib;
+                            int i = y * element * element + b * element + jb;
+                            if (j < ny && i < ny)
+                            {
+                                result[j * ny + i] = sums[a][b][ib ^ jb][jb];
+                            }
                         }
                     }
                 }
             }
         }
     }
-
-    delete[] data_copy;
 }
