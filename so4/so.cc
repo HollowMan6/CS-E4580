703f5a009d0ef48f4cd277463ee142045298200a
Hollow Man <hollowman@opensuse.org>
2023-05-16 23:09:41 +0300

    so4 final
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- so4/so.cc
+++ so4/so.cc
@@ -3,46 +3,15 @@
 
 typedef unsigned long long data_t;
 
-static inline int last_pow2(int x)
-{
-    return x == 1 ? 1 : 1 << ((64 - 1) - __builtin_clzl(x - 1));
-}
-
 void psort(int n, data_t *data)
 {
-    int threads = last_pow2(omp_get_max_threads());
+    int threads = omp_get_max_threads() * 99999;
+#pragma omp parallel for schedule(static, 1)
+    for (int i = 0; i < n; i += threads)
+        std::sort(data + i, data + std::min((i + threads), n));
 
-    if (threads < 2)
-        return std::sort(data, data + n);
-
-    int block = (n + threads - 1) / threads;
-
-#pragma omp parallel for
-    for (int i = 0; i <= threads; i++)
-    {
-        int index = i * block;
-        int start = std::min(index, n);
-        int end = std::min(index + block, n);
-        std::sort(data + start, data + end);
-    }
-
-    // merge pairs of blocks
-    threads /= 2;
-
-    while (threads > 0)
-    {
-#pragma omp parallel for
-        for (int i = 0; i < threads; i++)
-        {
-            int index = i * 2;
-
-            int start = index * block;
-            int middle = std::min(start + block, n);
-            int end = std::min(start + 2 * block, n);
-
-            std::inplace_merge(data + start, data + middle, data + end);
-        }
-        block *= 2;
-        threads /= 2;
-    }
+    for (int i = threads; i < n; i *= 2)
+#pragma omp parallel for schedule(static, 1)
+        for (int j = 0; j < n; j = j + 2 * i)
+            std::inplace_merge(data + j, data + std::min(j + i, n), data + std::min((j + 2 * i), n));
 }
