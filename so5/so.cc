4967e46cdda1944e5a19f8dc12200be8ccf441f1
Hollow Man <hollowman@opensuse.org>
2023-05-18 16:57:38 +0300

    Finish so5
    Signed-off-by: Hollow Man <hollowman@opensuse.org>

--- so5/so.cc
+++ so5/so.cc
@@ -1,9 +1,47 @@
 #include <algorithm>
+#include <random>
+#include <omp.h>
 
 typedef unsigned long long data_t;
 
-void psort(int n, data_t *data) {
-    // FIXME: Implement a more efficient parallel sorting algorithm for the CPU,
-    // using the basic idea of quicksort.
-    std::sort(data, data + n);
+data_t random(const int l, const int r)
+{
+    std::random_device rd;
+    static thread_local std::mt19937 rng(rd());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(l, r - 1);
+    return dist(rng);
+}
+
+void quicksort(data_t *d, data_t *l, data_t *r)
+{
+    int thread_max = omp_get_max_threads() * 4;
+
+    if (l >= r)
+        return;
+    else if (r - l < thread_max)
+    {
+        std::sort(l, r);
+        return;
+    }
+
+    data_t p = *(d + random(l - d, r - d));
+    data_t *m1 = std::partition(l, r, [p](const auto &n)
+                                { return n < p; });
+    data_t *m2 = std::partition(m1, r, [p](const auto &n)
+                                { return !(p < n); });
+
+#pragma omp task
+    quicksort(d, l, m1);
+
+#pragma omp task
+    quicksort(d, m2, r);
+}
+
+void psort(int n, data_t *d)
+{
+#pragma omp parallel
+#pragma omp single nowait
+    {
+        quicksort(d, d, d + n);
+    }
 }
