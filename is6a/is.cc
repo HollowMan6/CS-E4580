ce99155057f8a44f67dd3d79ada56dd724125f34
Songlin Jiang <songlin.jiang@csc.fi>
2023-05-23 20:04:02 +0300

    Improve is6a
    Signed-off-by: Songlin Jiang <songlin.jiang@csc.fi>

--- is6a/is.cc
+++ is6a/is.cc
@@ -1,180 +1,162 @@
 #include <vector>
-#include "vector.h"
 #include <x86intrin.h>
 
-using namespace std;
+typedef float float8_t __attribute__((vector_size(8 * sizeof(float))));
+
+float8_t zero{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
 struct Result
 {
-    int y0;
-    int x0;
-    int y1;
-    int x1;
-    float outer[3];
-    float inner[3];
+  int y0;
+  int x0;
+  int y1;
+  int x1;
+  float out[3];
+  float in[3];
 };
-
-float8_t max8(float8_t x, float8_t y)
-{
-    return x > y ? x : y;
-}
-
-float hmax(float8_t v)
-{
-    float r = 0;
-    for (int i = 0; i < 8; ++i)
-    {
-        if (v[i] > r)
-        {
-            r = v[i];
-        }
-    }
-    return r;
-}
 
 Result segment(int ny, int nx, const float *data)
 {
-    // pre compute sums of rectangles
-    vector<float> vda((nx + 1) * (ny + 1), 0.0);
+  std::vector<std::vector<float>> data_v(ny + 1, std::vector<float>(nx + 1));
 #pragma omp parallel for schedule(dynamic, 1)
-    for (int y = 1; y <= ny; y++)
-    {
-        for (int x = 1; x <= nx; x++)
-        {
-            vda[x + y * (nx + 1)] = data[3 * (x - 1) + 3 * nx * (y - 1)] + vda[(x - 1) + y * (nx + 1)];
-        }
-    }
-#pragma omp parallel for schedule(dynamic, 1)
+  for (int y = 1; y <= ny; y++)
+  {
     for (int x = 1; x <= nx; x++)
     {
-        for (int y = 1; y <= ny; y++)
+      data_v[y][x] = data[3 * (x - 1) + 3 * nx * (y - 1)] + data_v[y][x - 1];
+    }
+  }
+#pragma omp parallel for schedule(dynamic, 1)
+  for (int x = 1; x <= nx; x++)
+  {
+    for (int y = 1; y <= ny; y++)
+    {
+      data_v[y][x] += data_v[y - 1][x];
+    }
+  }
+
+  int size_p = ny * nx;
+  float sum = data_v[ny][nx];
+  float8_t sum_float8 = {sum, sum, sum, sum,
+                         sum, sum, sum, sum};
+
+  int wy_r = 1;
+  int wx_r = 1;
+
+  float best_u = -1;
+#pragma omp parallel for schedule(dynamic, 1)
+  for (int xy_w = 0; xy_w < nx * ny; xy_w++)
+  {
+    int x_w = xy_w % nx + 1;
+    int y_w = xy_w / nx + 1;
+    float size_w = y_w * x_w;
+    float size_wd = 1 / size_w;
+    float size_bd = 1 / (size_p - size_w);
+
+    float8_t max_float8 = zero;
+    float best = -1;
+    for (int y0 = 0; y0 <= ny - y_w; y0++)
+    {
+      int y1 = y0 + y_w;
+
+      int x0_b = nx - x_w + 1;
+      int blocks = x0_b / 8;
+      for (int b = 0; b < blocks; b++)
+      {
+        int x0_start = 8 * b;
+        int x1_start = x0_start + x_w;
+
+        float8_t s1 = _mm256_loadu_ps(&data_v[y1][x1_start]);
+        float8_t s2 = _mm256_loadu_ps(&data_v[y1][x0_start]);
+        float8_t s3 = _mm256_loadu_ps(&data_v[y0][x1_start]);
+        float8_t s4 = _mm256_loadu_ps(&data_v[y0][x0_start]);
+
+        float8_t v_in = s1 - s2 - s3 + s4;
+        float8_t v_out = sum_float8 - v_in;
+        float8_t v = v_in * v_in * size_wd + v_out * v_out * size_bd;
+        max_float8 = max_float8 > v ? max_float8 : v;
+      }
+
+      for (int x0 = 8 * blocks; x0 < x0_b; ++x0)
+      {
+        int x1 = x0 + x_w;
+
+        float sum_in =
+            data_v[y1][x1] - data_v[y1][x0] - data_v[y0][x1] + data_v[y0][x0];
+        float sum_out = sum - sum_in;
+        float util = sum_in * sum_in * size_wd + sum_out * sum_out * size_bd;
+        if (util > best)
         {
-            vda[x + y * (nx + 1)] += vda[x + (y - 1) * (nx + 1)];
+          best = util;
         }
+      }
     }
 
-    int psize = ny * nx;
-    float sum_all = vda[nx + ny * (nx + 1)];
-    float8_t f8psum = {
-        sum_all, sum_all, sum_all, sum_all,
-        sum_all, sum_all, sum_all, sum_all};
-
-    const float *vda2 = vda.data();
-
-    // best size
-    int r_wy = 1;
-    int r_wx = 1;
-
-    // find rect size with max utility
-    float util_best = -1;
-#pragma omp parallel for schedule(dynamic, 1)
-    for (int wxy = 0; wxy < nx * ny; wxy++)
+    float max_v = 0;
+    for (int i = 0; i < 8; ++i)
     {
-        int wx = wxy % nx + 1;
-        int wy = wxy / nx + 1;
-        float wsize = wy * wx;
-        float wsize1 = 1 / wsize;
-        float bsize1 = 1 / (psize - wsize);
-
-        float8_t f8max = float8_0;
-        float best = -1;
-        // each row = some blocks(float8_t) + some floats (<8)
-        for (int y0 = 0; y0 <= ny - wy; y0++)
-        {
-            int y1 = y0 + wy;
-
-            int x0bound = nx - wx + 1;
-            int blocks = x0bound / 8;
-            // vector operations in blocks
-            for (int block = 0; block < blocks; block++)
-            {
-                int x0_start = 8 * block;
-                int x1_start = x0_start + wx;
-
-                float8_t s1 = _mm256_loadu_ps(vda2 + y1 * (nx + 1) + x1_start);
-                float8_t s2 = _mm256_loadu_ps(vda2 + y1 * (nx + 1) + x0_start);
-                float8_t s3 = _mm256_loadu_ps(vda2 + y0 * (nx + 1) + x1_start);
-                float8_t s4 = _mm256_loadu_ps(vda2 + y0 * (nx + 1) + x0_start);
-
-                float8_t vin = s1 - s2 - s3 + s4;
-                float8_t vout = f8psum - vin;
-                float8_t v = vin * vin * wsize1 + vout * vout * bsize1;
-                f8max = max8(f8max, v);
-            }
-
-            // scalar operations
-            for (int x0 = 8 * blocks; x0 < x0bound; ++x0)
-            {
-                int x1 = x0 + wx;
-
-                float sumin = vda[y1 * (nx + 1) + x1] - vda[y1 * (nx + 1) + x0] - vda[y0 * (nx + 1) + x1] + vda[y0 * (nx + 1) + x0];
-                float sumout = sum_all - sumin;
-                float util = sumin * sumin * wsize1 + sumout * sumout * bsize1;
-                if (util > best)
-                {
-                    best = util;
-                }
-            }
-        }
-        // compare vecter max and scalar max
-        float vmax = hmax(f8max);
-        if (vmax > best)
-        {
-            best = vmax;
-        }
+      if (max_float8[i] > max_v)
+      {
+        max_v = max_float8[i];
+      }
+    }
+    if (max_v > best)
+    {
+      best = max_v;
+    }
 
 #pragma omp critical
-        {
-            if (best > util_best)
-            {
-                util_best = best;
-                r_wx = wx;
-                r_wy = wy;
-            }
-        }
-    }
-
-    int r_y0 = 0;
-    int r_x0 = 0;
-    int r_y1 = 0;
-    int r_x1 = 0;
-    float inner = 0;
-    float outer = 0;
-
-    float wsize = r_wx * r_wy;
-    float wsize1 = 1 / wsize;
-    float bsize1 = 1 / (psize - wsize);
-    // find best location of the rectangle
-    util_best = -1;
-#pragma omp parallel for schedule(dynamic, 1)
-    for (int y0 = 0; y0 <= ny - r_wy; y0++)
     {
-        for (int x0 = 0; x0 <= nx - r_wx; x0++)
-        {
-            int y1 = y0 + r_wy;
-            int x1 = x0 + r_wx;
+      if (best > best_u)
+      {
+        best_u = best;
+        wx_r = x_w;
+        wy_r = y_w;
+      }
+    }
+  }
 
-            float sumin = vda[y1 * (nx + 1) + x1] - vda[y1 * (nx + 1) + x0] - vda[y0 * (nx + 1) + x1] + vda[y0 * (nx + 1) + x0];
-            float sumout = sum_all - sumin;
-            float util = sumin * sumin * wsize1 + sumout * sumout * bsize1;
+  int y0_r = 0;
+  int x0_r = 0;
+  int y1_r = 0;
+  int x1_r = 0;
+  float in = 0;
+  float out = 0;
+
+  float size_w = wx_r * wy_r;
+  float size_wd = 1 / size_w;
+  float size_bd = 1 / (size_p - size_w);
+  best_u = -1;
+
+#pragma omp parallel for schedule(dynamic, 1)
+  for (int y0 = 0; y0 <= ny - wy_r; y0++)
+  {
+    for (int x0 = 0; x0 <= nx - wx_r; x0++)
+    {
+      int y1 = y0 + wy_r;
+      int x1 = x0 + wx_r;
+
+      float sum_in =
+          data_v[y1][x1] - data_v[y1][x0] - data_v[y0][x1] + data_v[y0][x0];
+      float sum_out = sum - sum_in;
+      float util = sum_in * sum_in * size_wd + sum_out * sum_out * size_bd;
 
 #pragma omp critical
-            {
-                if (util > util_best)
-                {
-                    util_best = util;
-                    r_y0 = y0;
-                    r_x0 = x0;
-                    r_y1 = y1;
-                    r_x1 = x1;
-                    inner = sumin * wsize1;
-                    outer = sumout * bsize1;
-                }
-            }
+      {
+        if (util > best_u)
+        {
+          best_u = util;
+          y0_r = y0;
+          x0_r = x0;
+          y1_r = y1;
+          x1_r = x1;
+          in = sum_in * size_wd;
+          out = sum_out * size_bd;
         }
+      }
     }
+  }
 
-    Result result = {
-        r_y0, r_x0, r_y1, r_x1, {outer, outer, outer}, {inner, inner, inner}};
-    return result;
+  Result result{y0_r, x0_r, y1_r, x1_r, {out, out, out}, {in, in, in}};
+  return result;
 }
